d18f3a2dededa47bc0663a8db951336ad468fdc8
Igor Knyazev <xaskell@yandex.ru>
2025-10-06 21:27:10 +0300

    ci: setup dockercompose and db settings

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=todo-mysql-1;dbname=yii2basic',
+    'username' => 'yii2basic',
+    'password' => 'secret',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
