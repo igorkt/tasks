1d9ebdc85896216ee498a237c64f8846d5794be1
Igor Knyazev <xaskell@yandex.ru>
2025-10-06 21:57:56 +0300

    feat: settings for api request and task controller

--- config/web.php
+++ config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'DJzDrHP5h5388Jd9ia2szQL1G8fzsnGh',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,14 +45,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'enableStrictParsing' => true,
+            'showScriptName' => true,
             'rules' => [
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'task'],
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
